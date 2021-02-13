bdefac40cb322becbc41ef0401b766504b3b528c
osmanforhad <osmanforhad93@gmail.com>
2021-02-13 16:26:35 +0600

    styling react native component

--- App.js
+++ App.js
@@ -4,9 +4,12 @@ import { Button, StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={(styles.container)}>
       <View>
-        <TextInput placeholder="Enter Todo Item"/>
+        <TextInput
+         placeholder="Enter Todo Item"
+           style={styles.textInput}
+         />
         <Button
           title="Add Todo"
           onPress={() => console.log("Button clicked")}
@@ -22,9 +25,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 60
+  },
+  textInput:{
+    padding:10,
+    borderColor:'#000000',
+    marginBottom:10,
+    borderWidth:1,
   },
 });
