10dc98d70c89ea2234aa94c62bf20785e491a678
osmanforhad <osmanforhad93@gmail.com>
2021-02-13 15:50:14 +0600

    working with component

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ACCL Mobile APPS!</Text>
+      <View>
+        <TextInput placeholder="Enter Todo Item"/>
+        <Button
+          title="Add Todo"
+          onPress={() => console.log("Button clicked")}
+        />
+      </View>
+      <View>
+        <Text>List of To dos</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
